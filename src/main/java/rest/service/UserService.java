d17204324180dc93c2fac383b55eac209fee8a9f
BrizziB <boris.brizzi@gmail.com>
2019-03-12 19:46:15 +0100

    inseriti primi servizi per dettagli user

--- src/main/java/rest/service/UserService.java
+++ src/main/java/rest/service/UserService.java
@@ -1,11 +1,10 @@
 package rest.service;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,7 +12,8 @@ import javax.ws.rs.core.Response;
 import com.google.gson.Gson;
 
 import bean.UserSessionBean;
-import model.Product;
+import dao.UserDao;
+import model.User;
 
 
 @Path("user")
@@ -21,6 +21,9 @@ public class UserService {
 	
 	@Inject
 	private UserSessionBean loggedUser;
+	
+	@Inject
+	private UserDao userDao;
 	
 	
 	@GET
@@ -39,6 +42,47 @@ public class UserService {
 			} else {
 				resp = Response.ok(gson.toJson(null), MediaType.APPLICATION_JSON).build();
 			}
+			return resp;
+		}catch(Exception e) {
+			
+			return Response.notAcceptable(null).build();
+		}
+	}
+	
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/data")
+	@Produces( { "application/json", "text/plain" } )
+	/* 
+	 * comunica a un eventuale client se la sua sessione è ancora attiva sul server, in tal caso invia nuovamente lo userID, altrimenti null
+	 * */
+	public Response getUserDataStateful() throws Exception{
+		Gson gson = new Gson();
+		try {
+			Response resp;
+			System.out.println("rispondo a richiesta di getUserData");
+			User user = userDao.findById(loggedUser.getUserId());
+			resp = Response.ok(gson.toJson(user), MediaType.APPLICATION_JSON).build();
+			return resp;
+		}catch(Exception e) {
+			
+			return Response.notAcceptable(null).build();
+		}
+	}
+	
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/data/{userID}")
+	@Produces( { "application/json", "text/plain" } )
+	/* 
+	 * comunica a un eventuale client se la sua sessione è ancora attiva sul server, in tal caso invia nuovamente lo userID, altrimenti null
+	 * */
+	public Response getUserDataStateless(@PathParam("userID") Long userID) throws Exception{
+		Gson gson = new Gson();
+		try {
+			Response resp;
+			User user = userDao.findById(userID);
+			resp = Response.ok(gson.toJson(user), MediaType.APPLICATION_JSON).build();
 			return resp;
 		}catch(Exception e) {
 			
